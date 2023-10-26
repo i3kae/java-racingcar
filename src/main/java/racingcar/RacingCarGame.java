3f3702a8b9dfb6ba196ae9628b4e9ed694ae095b
i3kae <snoger1234@gmail.com>
2023-10-26 17:03:43 +0900

    feat : RacingCarGame 클래스 생성 및 이름 입력 기능 구현 1. RacingCarGame 클래스 생성을 진행해 코드 모듈화 진행 2. 이름 readLine을 통한 쉼표 구분 전 이름 입력 진행

---
+++ src/main/java/racingcar/RacingCarGame.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Console;
+public class RacingCarGame {
+    private ArrayList<String> names;
+    public void start(){
+        String originalStr = "";
+        originalStr = Console.readLine();
+        System.out.println(originalStr);
+    }
+}
