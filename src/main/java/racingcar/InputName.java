ac06461145d1887e2eef07f9bdc9d3f3f70fbd12
i3kae <snoger1234@gmail.com>
2023-10-28 00:36:01 +0900

    feat, refactor : 이름 무결성 검사 추가, 문자열 함수 클래스 이동 1. 입력 받은 이름들에 대한 무결성 검사를 진행하는 클래스와 코드를 작성하였습니다. 2. commaSplit, commaCheck 함수를  StringUtils 클래스로 이동하였습니다.

--- src/main/java/racingcar/InputName.java
+++ src/main/java/racingcar/InputName.java
@@ -4,33 +4,21 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class InputName {
-    public ArrayList<String> inputName(){
-        ArrayList<String> names;
-        String originalStr = Console.readLine();
-        names = commaSplit(originalStr);
+    public ArrayList<Racingcar> inputName(){
+        String[] names;
+        ArrayList<Racingcar> cars = new ArrayList<>();
+        StringUtils strUtils = new StringUtils();
+        IntegrityCheck integrityCheck = new IntegrityCheck();
+        String originalStr = "";
 
-        return names;
-    }
+        originalStr = Console.readLine();
+        names = strUtils.commaSplit(originalStr);
 
-    public ArrayList<String> commaSplit(String originalStr){
-        ArrayList<String> names = new ArrayList<>();
-
-        if (commaCheck(originalStr) != 2){
-            return null;
-        }
         for (int i = 0; i < 3; i++){
-            names.add(originalStr.substring(originalStr.lastIndexOf(",")+1));
+            if (integrityCheck.nameIntegrityCheck(names[i])){
+                cars.add(new Racingcar(names[i]));
+            }
         }
-
-        return names;
-    }
-    public int commaCheck(String str){
-        int commaNum = 0;
-
-        for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) == ',')
-                commaNum++;
-        }
-        return commaNum;
+        return cars;
     }
 }
