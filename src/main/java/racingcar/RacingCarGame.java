7efe6838436833f087d3653d974eb10f77243f2b
i3kae <snoger1234@gmail.com>
2023-10-28 00:41:02 +0900

    feat : 이름 입력 진행 후 보관 1. 이름 입력을 진행하고 난 다음 cars ArrayList에 담아주었습니다.

--- src/main/java/racingcar/RacingCarGame.java
+++ src/main/java/racingcar/RacingCarGame.java
@@ -3,12 +3,9 @@ package racingcar;
 import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Console;
 public class RacingCarGame {
-    private ArrayList<String> names;
     public void start(){
-        String originalStr = "";
-        StringUtils strUtils = new StringUtils();
-
-        originalStr = Console.readLine();
-        names = strUtils.commaSplit(originalStr);
+        InputName inputName = new InputName();
+        ArrayList<Racingcar> cars;
+        cars = inputName.inputName();
     }
 }
